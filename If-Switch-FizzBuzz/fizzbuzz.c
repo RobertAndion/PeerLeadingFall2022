088a22aad400028ffa6f7ccacc82dc798622605a
Robert <randion@usf.edu>
2021-09-03 16:39:05 -0400

    FizzBuzz Solution

--- If-Switch-FizzBuzz/fizzbuzz.c
+++ If-Switch-FizzBuzz/fizzbuzz.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
+#include <math.h>
 
 #define TRUE 1
 #define FALSE 0
+
+// FizzBuzz challange. Print 1-20 but every number divisible by 3 is replaced with fizz
+// and any number divisible by 5 is replaced with buzz, numbers divisible by both are
+// replaced with FizzBuzz (9-3-21)
 
 int main()
 {
     int i;
     // Start of FizzBuzz Program
-    for (i = 1; i < 21; i++)
+    for (i = 1; i < 101; ++i)
     {
+        if(i % 3 == 0 && i % 5 == 0) { // If Else switch version
+            printf("FizzBuzz\n");
+        } else if (i % 3 == 0) {
+            printf("Fizz\n");
+        } else if (i % 5 == 0) {
+            printf("Buzz\n");
+        }
+        /*
+        // if only version
+        if(i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz\n");
+        }
+        if(i % 3 == 0) {
+            printf("Fizz\n");
+            continue;
+        }
+
+        if(i % 5 == 0) {
+            printf("Buzz\n");
+            continue;
+        }*/
+
+        printf("%d\n", i);
     }
 
     return 0;
 }
-
-// If statements
-
-// Booleans and how they work in C
-
-// Loops with continue and break
-
-// FizzBuzz challange. Print 1-20 but every number divisible by 3 is replaced with fizz
-// and any number divisible by 5 is replaced with buzz, numbers divisible by both are
-// replaced with FizzBuzz
